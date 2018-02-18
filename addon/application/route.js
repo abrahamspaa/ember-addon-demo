3eb84b64a442e7316de9a4ef0d6f327b2cc13c53
Paul <paulashwinabraham.samuel@scb.com>
2018-02-18 09:11:03 +0530

    added controller

---
+++ addon/application/route.js
@@ -0,0 +1,4 @@
+import AuthenticatedApplicationRoute from 'wb-ui-core/routes/authenticated-application';
+
+export default AuthenticatedApplicationRoute.extend({
+});
---
+++ app/application/template.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-addon-demo/application/template';
