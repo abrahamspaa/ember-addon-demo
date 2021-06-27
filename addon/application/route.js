e9f00a4dbf201bc981e73e98fd84bc7dbc73cf49
Paul Ashwin <abrahamspaa@gmail.com>
2021-06-27 18:53:20 +0530

    removed unwanted changes

--- addon/application/route.js
+++
@@ -1,4 +0,0 @@
-import AuthenticatedApplicationRoute from 'wb-ui-core/routes/authenticated-application';
-
-export default AuthenticatedApplicationRoute.extend({
-});
--- app/application/template.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-addon-demo/application/template';
